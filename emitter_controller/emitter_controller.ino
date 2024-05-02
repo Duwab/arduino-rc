6711a34051341e9f94c500aad7be35e215cfebda
Duwab <Duwab@users.noreply.github.com>
2024-05-02 15:38:56 +0200

    Send struct data

--- emitter_controller/emitter_controller.ino
+++ emitter_controller/emitter_controller.ino
@@ -13,6 +13,17 @@ Sensors mySensors; // no brackets when no args
 //Sensors mySensors(arg1, arg2);
 Transmitter transmitter;
 
+struct ControllerData {
+  uint8_t x; // 1o
+  uint8_t y; // 1o
+  uint8_t z; // 1o
+  bool sw;   // 1o
+  int other; // 2o
+             //=6o (max 32o)
+};
+
+ControllerData controllerData = { 0, 0, 0, 0, 0 };
+
 void setup() {
   Serial.begin(115200);
   blinkUtil.init();
@@ -27,9 +38,20 @@ void loop() {
   mySensors.logValues();
   transmitter.sendMessage("sendA");
   transmitter.sendMessage("sendB");
+
+  readControllerData();
+  transmitter._radio.write(&controllerData, sizeof(controllerData));
   delay(300);
 
   //if (radio.available()) {        // On vérifie si un message est en attente de lecture
   //  radio.read(&message, sizeof(message));             // Si oui, on le charge dans la variable "message"
   //}
+}
+
+void readControllerData() {
+  controllerData.x = mySensors.getX();
+  controllerData.y = mySensors.getY();
+  controllerData.z = mySensors.getZ();
+  controllerData.sw = mySensors.getSW();
+  controllerData.other = 12;
 }
