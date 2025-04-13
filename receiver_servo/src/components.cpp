22a3b44e47ad46c67185a341b283b33f7538d5f9
Duwab <Duwab@users.noreply.github.com>
2025-04-13 11:22:25 +0200

    Add Servo 3 control

--- receiver_servo/src/components.cpp
+++ receiver_servo/src/components.cpp
@@ -2,14 +2,16 @@
 #include "components.h"
 #include <Servo.h>
 
-#define pinSERVO1  6
-#define pinSERVO2  9
-#define pinSERVOESC  10
+#define pinSERVOESC 3
+#define pinSERVO1   5
+#define pinSERVO2   6
+#define pinSERVO3   9
 
 Servo servo1;
 int angleServo1;
 Servo servo2;
 int angleServo2;
+Servo servo3;
 Servo servoESC;
 
 #define dureeMinimaleImpulsionCommandeESC     1000        // La durée minimale pour une impulsion est de 1000 µs, soit 1 ms (comme pour un servomoteur, en fait)
@@ -27,7 +29,8 @@ void Components::init()
 {
     servo1.attach(pinSERVO1);
     servo2.attach(pinSERVO2);
-    servoESC.attach(pinSERVOESC);
+    servo3.attach(pinSERVO3);
+    servoESC.attach(pinSERVOESC, dureeMinimaleImpulsionCommandeESC, dureeMaximaleImpulsionCommandeESC);
     servo1.write(0);
     delay(DELAY_BETWEEN_SERVO);
     servo2.write(0);
@@ -49,6 +52,11 @@ void Components::setServo1(uint8_t angle, int rangeMin, int rangeMax) {
 void Components::setServo2(uint8_t angle, int rangeMin, int rangeMax) {
     angleServo2 = map(angle, rangeMin, rangeMax, 0, 180);
     servo2.write(angleServo2);
+}
+
+void Components::setServo3(uint8_t angle, int rangeMin, int rangeMax) {
+    int angleServo3 = map(angle, rangeMin, rangeMax, 0, 180);
+    servo3.write(angleServo3);
 }
 
 void Components::setServoESC(uint8_t angle, int rangeMin, int rangeMax) {
